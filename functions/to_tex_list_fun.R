cb833c5bc21c0a13a4c2aa8f1f320a69961823df
Arnald Puy <you@example.com>
2026-01-02 13:25:42 +0000

    corrected language thing

--- functions/to_tex_list_fun.R
+++ functions/to_tex_list_fun.R
@@ -24,7 +24,7 @@ to_tex_list_fun <- function(lst) {
     paths <- escape_latex(paths)
     paths <- gsub("→", "\\\\textrightarrow{}", paths)  
     
-    cat(sprintf("\\paragraph{%s}\n", title))
+    cat(sprintf("\\subsection{%s}\n", title))
     cat("\\begin{enumerate}[leftmargin=*, itemsep=0.25em]\n")
     cat(paste0("\\item {\\small\\ttfamily ", paths, "}\n", collapse = ""))
     cat("\\end{enumerate}\n\n")
